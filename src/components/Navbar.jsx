cd14e32b657d35cc1de00de12ae270ca33d14a9b
KristianColville1 <kristiancolville1@gmail.com>
2023-04-28 19:17:51 +0100

    Added screen size adjustment for smaller devices to hide sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,8 +28,22 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 );
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
-    const handleClick = () => {};
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.width);
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, [])
+    useEffect(() => {
+        if (screenSize <= 900) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize])
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
             <NavButton
@@ -81,6 +95,11 @@ const Navbar = () => {
                         <MdKeyboardArrowDown className="text-gray-400 text-14" />
                     </div>
                 </TooltipComponent>
+
+                {isClicked.cart && <Cart />}
+                {isClicked.chat && <Chat />}
+                {isClicked.notification && <Notification />}
+                {isClicked.userProfile && <UserProfile />}
             </div>
         </div>
     );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import { SiShopware } from "react-icons/si";
+import { SiCaffeine } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
@@ -8,7 +8,12 @@ import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
+    const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSideBar = () => {
+      if (activeMenu !== undefined && screenSize <= 900) {
+          setActiveMenu(false);
+      }
+  };
 
     const activeLink =
         "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md-m2";
@@ -21,10 +26,10 @@ const Sidebar = () => {
                     <div className="flex justify-between items-center">
                         <Link
                             to="/"
-                            onClick={() => setActiveMenu(false)}
+                            onClick={handleCloseSideBar}
                             className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
                         >
-                            <SiShopware /> <span>Shoppy</span>
+                            <SiCaffeine /> <span>EMS</span>
                         </Link>
                         <TooltipComponent
                             content="Menu"
@@ -49,7 +54,7 @@ const Sidebar = () => {
                                     <NavLink
                                         to={`/${link.name}`}
                                         key={link.name}
-                                        onClick={() => {}}
+                                        onClick={handleCloseSideBar}
                                         className={({ isActive }) =>
                                             isActive ? activeLink : normalLink
                                         }

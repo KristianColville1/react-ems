e1ad73724f742da720d86d979e95956f89fe874e
KristianColville1 <kristiancolville1@gmail.com>
2023-04-29 09:07:12 +0100

    Fix bug for sidebar showing on small screens

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,18 +9,18 @@ import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
     const { activeMenu, setActiveMenu, screenSize } = useStateContext();
-  const handleCloseSideBar = () => {
-      if (activeMenu !== undefined && screenSize <= 900) {
-          setActiveMenu(false);
-      }
-  };
+    const handleCloseSideBar = () => {
+        if (activeMenu !== undefined && screenSize <= 900) {
+            setActiveMenu(false);
+        }
+    };
 
     const activeLink =
-        "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md-m2";
+        "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
     const normalLink =
         "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
     return (
-        <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pd-10">
+        <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
             {activeMenu && (
                 <>
                     <div className="flex justify-between items-center">
@@ -37,7 +37,11 @@ const Sidebar = () => {
                         >
                             <button
                                 type="button"
-                                onClick={() => setActiveMenu(prevActiveState => !prevActiveState)}
+                                onClick={() =>
+                                    setActiveMenu(
+                                        (prevActiveState) => !prevActiveState
+                                    )
+                                }
                                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
                             >
                                 <MdOutlineCancel />
@@ -47,7 +51,7 @@ const Sidebar = () => {
                     <div className="mt-10">
                         {links.map((item) => (
                             <div key={item.title}>
-                                <p className="text-gray-400 m-3 mt-4 uppercase">
+                                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
                                     {item.title}
                                 </p>
                                 {item.links.map((link) => (

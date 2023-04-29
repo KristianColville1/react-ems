45ac6099a890b939f525bf673ca48bc8cf41db52
KristianColville1 <kristiancolville1@gmail.com>
2023-04-29 19:31:23 +0100

    Added calendar page for events

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+    ScheduleComponent,
+    ViewsDirective,
+    ViewDirective,
+    Day,
+    Week,
+    WorkWeek,
+    Month,
+    Agenda,
+    Inject,
+    Resize,
+    DragAndDrop,
+} from "@syncfusion/ej2-react-schedule";
+import { DatePickerComponent } from "@syncfusion/ej2-react-calendars";
 
-const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
-export default Calendar
+const PropertyPane = (props) => <div className="mt-5">{props.children}</div>;
+
+const Scheduler = () => {
+    const [scheduleObj, setScheduleObj] = useState();
+
+    const change = (args) => {
+        scheduleObj.selectedDate = args.value;
+        scheduleObj.dataBind();
+    };
+
+    const onDragStart = (arg) => {
+        arg.navigation.enable = true;
+    };
+
+    return (
+        <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+            <Header category="App" title="Calendar" />
+            <PropertyPane >
+                <table style={{ width: "100%", background: "white" }}className="p-2">
+                    <tbody>
+                        <tr style={{ height: "50px" }}>
+                            <td style={{ width: "100%" }} className="bg-blue-50 px-3 py-2">
+                                <DatePickerComponent
+                                    value={new Date()}
+                                    showClearButton={false}
+                                    placeholder="Current Date"
+                                    floatLabelType="Always"
+                                    change={change}
+                                    
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </PropertyPane>
+            <ScheduleComponent
+                height="650px"
+                ref={(schedule) => setScheduleObj(schedule)}
+                selectedDate={new Date()}
+                eventSettings={{ dataSource: scheduleData }}
+                dragStart={onDragStart}
+            >
+                <ViewsDirective>
+                    {["Day", "Week", "WorkWeek", "Month", "Agenda"].map(
+                        (item) => (
+                            <ViewDirective key={item} option={item} />
+                        )
+                    )}
+                </ViewsDirective>
+                <Inject
+                    services={[
+                        Day,
+                        Week,
+                        WorkWeek,
+                        Month,
+                        Agenda,
+                        Resize,
+                        DragAndDrop,
+                    ]}
+                />
+            </ScheduleComponent>
+        </div>
+    );
+};
+
+export default Scheduler;

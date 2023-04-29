ba7202281b575b72084f1492e595d496443147d4
KristianColville1 <kristiancolville1@gmail.com>
2023-04-29 09:29:27 +0100

    Added structure for ecommerce page content and first button

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { BiCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { stacked, Pie, Button, SparkLine } from '../components';
+import { earningData, SparkedlineAreaData, ecomPieChartDat } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
   return (
-    <div>Ecommerce</div>
-  )
+      <div className="mt-24">
+          <div className="flex flex-wrap lg:flex-nowrap justify-center">
+              <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+                  <div className="flex justify-between items-center">
+                      <div>
+                          <p className="font-bold text-gray-400">Earnings</p>
+                          <p className="text-2xl">€20,000</p>
+                      </div>
+                  </div>
+                  <div className="mt-6">
+                      <Button color="white" bgColor="blue" text="Download" borderRadius="10px" size="md"/>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
 }
 
 export default Ecommerce
